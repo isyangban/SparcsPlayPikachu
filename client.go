a5e5176e136f90e3c54ade7497f231720483d413
Jeong Woon Choi <choijeongwoon@gmail.com>
2015-11-01 16:14:44 +0900

    Changed: send only appointed commands

--- client.go
+++ client.go
@@ -7,6 +7,18 @@ import (
 	"net"
 )
 
+var kKeyMap = map[string]string{
+	"a":     "key a",
+	"b":     "key b",
+	"x":     "key x",
+	"y":     "key y",
+	"up":    "key up",
+	"down":  "key down",
+	"left":  "key left",
+	"right": "key right",
+	"nop":   "",
+}
+
 func main() {
 	server_addr := flag.String("d", "", "server ip address")
 	command := flag.String("c", "", "remote command")
@@ -17,11 +29,25 @@ func main() {
 		fmt.Println("Error: ", err)
 		return
 	}
-	fmt.Fprintf(conn, "GET /%v HTTP/1.0\r\n\r\n", *command)
+	if check_command(*command) {
+		fmt.Fprintf(conn, "GET /%v HTTP/1.0\r\n\r\n", *command)
+	} else {
+		return
+	}
 	status, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
-	fmt.Println("Response:", status)
+	fmt.Print("Response:", status)
+}
+
+func check_command(c string) bool {
+	text, ok := kKeyMap[c]
+	if ok {
+		fmt.Println(text + " sent")
+	} else {
+		fmt.Println("Error: unknown key " + c)
+	}
+	return ok
 }
